5c360e0ad774439b653f95dffe3a6ccdb836f61b
pratiksontakke <pratikass488@gmail.com>
2023-09-10 20:12:16 +0530

    localStorage feature

--- 01_react/src/components/App.js
+++ 01_react/src/components/App.js
@@ -1,30 +1,40 @@
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactCard from "./ContactCard";
 import ContactList from "./ContactList";
 
-
 function App() {
 
-  const contacts = [
-    {
-      id: "1",
-      "name": "Dipesh",
-      "email": "malvia@gmail.com"
-    },
-    {
-      id: "2",
-      "name": "Nikesh",
-      "email": "nicks@gmail.com"
-    }
-  ]
+  // const contacts = [
+  //   {
+  //     id: "1",
+  //     "name": "Dipesh",
+  //     "email": "malvia@gmail.com"
+  //   },
+  //   {
+  //     id: "2",
+  //     "name": "Nikesh",
+  //     "email": "nicks@gmail.com"
+  //   }
+  // ]
+
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState(() => { return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [] });
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
-    <div>
+    <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler} />
       <ContactList contacts={contacts} />
       {/* <ContactCard /> */}
     </div>
